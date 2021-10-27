3cfbf3fc2d75cfa26cec1811d68a92d834d91a05
MORIMORI0317 <morimori.0317@outlook.jp>
2021-10-27 19:08:12 +0900

    fix forge

--- forge/src/main/java/net/morimori0317/simpletranslation/impl/foge/SimpleTranslationExpectPlatformImpl.java
+++ forge/src/main/java/net/morimori0317/simpletranslation/impl/foge/SimpleTranslationExpectPlatformImpl.java
@@ -1,4 +1,4 @@
-package net.morimori0317.simpletranslation.impl;
+package net.morimori0317.simpletranslation.impl.foge;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
